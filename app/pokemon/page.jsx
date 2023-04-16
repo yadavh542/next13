9b7f9990f8dbdc831d2500225797f2dc5ff59151
Hemant <yadavh542@gmail.com>
2023-04-16 20:49:14 +0530

    change3

--- app/pokemon/page.jsx
+++ app/pokemon/page.jsx
@@ -1,16 +1,22 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import PokemonTable from '../components/PokemonTable';
 import {store} from "../store";
 import { setStartupPokemon } from '../store/searchSlice';
 import SSRPokemonTable from '../components/SSRPokemonTable';
 import SearchInput from '../components/SearchInput';
 
-const PokemonPage = async() => {
+const PokemonPage = () => {
+
+    const getData=async() => {
     const req = await fetch("http://localhost:3000/api/search");
     const data = await req.json();
     store.dispatch(setStartupPokemon(data));
+  };
+    useEffect(()=>{
+      getData();
+    },[])
 
   return (
     <div className='h-full w-full'>
